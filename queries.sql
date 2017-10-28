20994c51f7531d201e51041ff4105f424af6faec
beelarr <bryonl@me.com>
2017-10-27 20:01:30 -0500

    Through 7

--- queries.sql
+++ queries.sql
@@ -2,3 +2,6 @@ SELECT FirstName, LastName, CustomerId, Country FROM Customer where Country != '
 SELECT FirstName, LastName, CustomerId, Country FROM Customer where Country = 'Brazil'
 SELECT FirstName, LastName, InvoiceId, Invoice.InvoiceDate, Country FROM Customer  JOIN Invoice on Customer.CustomerId = Invoice.CustomerId where Customer.Country = 'Brazil'
 select * from employee where title = 'Sales Support Agent'
+select distinct BillingCountry from Invoice
+select Employee.FirstName, Employee.LastName, Invoice.InvoiceId, Invoice.Total from Employee join Customer on Customer.SupportRepId = Employee.EmployeeId  join Invoice on Customer.CustomerId = Invoice.CustomerId
+select Invoice.Total, Customer.FirstName, Customer.LastName, Customer.Country, Employee.FirstName, Employee.LastName from Invoice join Customer on Invoice.CustomerId = Customer.CustomerId join Employee on  Customer.SupportRepId = Employee.EmployeeId
